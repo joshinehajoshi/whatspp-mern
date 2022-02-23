833a6bda1b0ca9edfd40e0d7764681e85ffa7b9d
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-23 19:49:39 +0530

    sidebar comp added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div className="App">
     <h1>Hello</h1>
+    <SideBar />
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styling/sidebar.css';
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+        <h1>SideBar</h1>
+    </div>
+  )
+}
+
+export default SideBar
