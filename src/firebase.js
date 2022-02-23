3e875b7d5aeae431d0782dacbea8030e6b4032e8
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-23 18:14:20 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7drj6uaSQs1_pGphwkxZoq0vYeAhzesI",
+    authDomain: "whatspp-mern-21e8f.firebaseapp.com",
+    projectId: "whatspp-mern-21e8f",
+    storageBucket: "whatspp-mern-21e8f.appspot.com",
+    messagingSenderId: "15426812777",
+    appId: "1:15426812777:web:f1f3e0795a9decef23dbf8"
+  };
