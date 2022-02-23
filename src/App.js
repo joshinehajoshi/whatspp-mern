f556bb88f4a84fddae78f7efe9d320513bb7c278
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-23 19:51:55 +0530

    chat comp added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SideBar from './components/SideBar';
+import Chat from './components/Chat';
 
 function App() {
   return (
     <div className="App">
     <h1>Hello</h1>
     <SideBar />
+    <Chat />
     </div>
   );
 }
