f54336bf57e81487bbf99441ef1e7b721703b69e
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-25 00:01:29 +0530

    iconbutton added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../styling/sidebar.css';
 import ChatIcon from '@mui/icons-material/Chat';
+import { Avatar,IconButton } from '@mui/material';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 
 function SideBar() {
   return (
     <div className="sidebar">
-        <h1>SideBar</h1>
         <div className="sidebar_header">
           <div className="sidebar_headerRight">
-            <DonutLargeIcon />
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
           </div>
         </div>
     </div>
